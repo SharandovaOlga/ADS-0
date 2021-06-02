8587fd436ae0d30832b606e180ccde69f7d48ceb
SharandovaOlga <75723905+SharandovaOlga@users.noreply.github.com>
2021-06-02 16:08:56 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -2,6 +2,11 @@
 #ifndef INCLUDE_ALG_H_
 #define INCLUDE_ALG_H_
 
-int gcd(int a, int b);
+int gcd(int a, int b) {
+ if (b == 0)
+  return a;
+ else
+  return gcd (b, a % b);
+}
 
 #endif  // INCLUDE_ALG_H_
